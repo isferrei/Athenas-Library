0c764b01d26fb2a6ed17c6938afbe27c054455d1
isferrei <54048443+isferrei@users.noreply.github.com>
2019-12-03 10:39:24 -0300

    Add files via upload

---
+++ Athenas library.php
@@ -0,0 +1,34 @@
+﻿<!doctype html>
+<html>
+<head>
+<link href="css2/estilo.css" rel="stylesheet" type="text/css">
+<meta charset="utf-8">
+<title>Athenas library</title>
+</head>
+<body>
+<center>
+<div id="claro">
+<img src="pws-logo.fw.png" width="222" height="231">
+</div>
+<div id="escuro">
+<img src="POWERSYSTEM2.fw.png" width="500" height="150">
+</div>
+<div id="aempresa">
+Empresa destinada ao desenvolvimento de sistemas e websites.
+</div>
+	
+<div class="athenas">
+<a href="DESIGN/INDEX.php">	
+<img src="DESIGN/images/athenasbt.fw.png">
+</a>
+<a href="Sistema/index1.php">	
+<img src="DESIGN/images/athenasbt2.fw.png">
+</a>
+</div>
+
+	
+
+
+
+</body>
+</html>
